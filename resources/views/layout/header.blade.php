e2338f577a560a60507ff8e5ce4f60629a8403ef
restuhanputra <restu.hanputraa@gmail.com>
2020-09-06 13:45:35 +0700

    add layout.header

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet') }}" type="text/css">
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+    @yield('css')
+</head>
+
+
+
+
+
