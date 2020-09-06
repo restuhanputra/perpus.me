b50d2aed29f94c7e5325d3ae1b788327fdbe1dc9
restuhanputra <restu.hanputraa@gmail.com>
2020-09-06 12:11:27 +0700

    add view->login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <meta name="description" content=""> --}}
+    {{-- <meta name="author" content=""> --}}
+
+    <title>{{ config('app.name', 'Laravel') }} - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary" style="margin-top: 4%;">
+
+    <style>
+    .bg-login-image {
+        background: url({{ asset('admin/img/books-with-laptop.jpg') }});
+        background-position: center;
+        background-size: cover;
+    }
+    </style>
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center text-bold">
+                                        <h1 class="h4 text-gray-900 mb-4">{{ config('app.name', 'Laravel') }}</h1>
+                                    </div>
+                                    <form class="user" action="process" method="POST">
+                                    {{ csrf_field() }}
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                name="email" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                name="password" placeholder="Password">
+                                        </div>
+                                        <br>
+
+                                        <button class="btn btn-primary btn-user btn-block" type="submit">Login</button>
+                                    </form>
+                                    <hr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
+
+</body>
+
+</html>
