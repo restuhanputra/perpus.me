8e1144524954caa496b21c705391dbe296515213
reptr <restuyoichi@gmail.com>
2020-09-06 16:51:46 +0700

    add view->kategori

---
+++ resources/views/admin/kategori/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layout.wrapper')
+@section('title')
+    {{ $data['title'] }}
+@endsection
+@section('content')
+<div class="row">
+
+    <div class="col-sm-12">
+        <div class="card shadow mb-4">
+            <!-- Card Header - Dropdown -->
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <a href="/kategori/add" class="btn btn-primary btn-icon-split">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-plus"></i>
+                    </span>
+                    <span class="text">Tambah Kategori</span>
+                </a>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Kategori</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
