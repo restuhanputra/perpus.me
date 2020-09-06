01c18f218c9aa5c849ffe949ab5ecf7be2202481
reptr <restuyoichi@gmail.com>
2020-09-06 13:45:09 +0700

    add layout.content

---
+++ resources/views/layout/content.blade.php
@@ -0,0 +1,22 @@
+<!-- Main Content -->
+<div id="content">
+
+    <!-- Topbar -->
+    @include('layout.topbar')
+    <!-- End of Topbar -->
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        </div>
+
+        @yield('content')
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
