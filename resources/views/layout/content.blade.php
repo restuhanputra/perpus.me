e8039825497a2df3c99f2c93ad8dc534dbeb8cdd
reptr <restuyoichi@gmail.com>
2020-09-06 16:50:29 +0700

    add yield(title)

--- resources/views/layout/content.blade.php
+++ resources/views/layout/content.blade.php
@@ -10,7 +10,7 @@
 
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <h1 class="h3 mb-0 text-gray-800">@yield('title')</h1>
         </div>
 
         @yield('content')
